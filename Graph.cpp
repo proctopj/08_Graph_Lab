690ceed58370a964a7449f23ec5b3ce5afd4c072
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-10-03 11:23:54 +0930

    Initial commit

---
+++ Graph.cpp
@@ -0,0 +1,27 @@
+/*****
+ * Author   : brinkmwj
+ * Date     : 2009-11-06
+ * Sources  : All code is original
+ */
+#include "Graph.h"
+
+Graph::Graph(unsigned int numNodes){
+  //TODO
+}
+
+int Graph::getCost(int node1, int node2){
+  //TODO
+  return -1;
+}
+
+//Add an edge from node1 to node2, and from node2 to node1, with
+// the given cost. If the cost is < 0, throw a string exception.
+void Graph::addEdge(int node1, int node2, double cost){
+  //TODO
+}
+
+//Remove the edge from node1 to node2, and also from node2 to node1.
+// If there are no such edges, then don't do anything.
+void Graph::removeEdge(int node1, int node2){
+  //TODO
+}
